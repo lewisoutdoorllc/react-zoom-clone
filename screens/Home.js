05fa59e552572ee949e1751c98a01cdb9d7eae46
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-10-23 22:24:23 -0400

    set up starting files

---
+++ screens/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function Home() {
+    return (
+        <View>
+            {/* header */}
+            {/* searchBar */}
+            {/* menuButtons */}
+            {/*  contactsMenu */}
+            <>
+                <div>
+                    <h1>Home</h1>
+                </div>
+            </>
+        </View>
+    )
+}
+
+export default Home
