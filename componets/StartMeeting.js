0bce4d8cb0a30e55493c164642927d4298d88129
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-11-05 18:59:57 -0400

    cleaned up and moved the start meeting views from the meeting room to the start meeting to clean up files; started setting up and implementing socket.io

--- componets/StartMeeting.js
+++ componets/StartMeeting.js
@@ -1,11 +1,69 @@
 import React from 'react'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 
-export default function StartMeeting() {
+export default function StartMeeting({ name, roomId, setRoomId, setName }) {
     return (
-        <div>
-            
-        </div>
+        <View>
+            <View style={styles.startMeetingsContainer}>
+                <View style={styles.info}>
+                    <TextInput
+                        style={styles.textInput}
+                        value={name}
+                        placeholder='Enter Name'
+                        placeholderTextColor='#9b9b9b'
+                        onChangeText={text => setName(text)}
+                    />
+                </View>
+                <View style={styles.info}>
+                    <TextInput
+                        style={styles.textInput}
+                        value={roomId}
+                        placeholder='Enter A Room ID'
+                        placeholderTextColor='#9b9b9b'
+                        onChangeText={text => setRoomId(text)}
+                    />
+                </View>
+                {/* Button */}
+                <View style={{ alignItems: 'center' }}>
+                    <TouchableOpacity
+                        style={styles.startMeetingBtn}
+                        onPress={() => { }}
+                    >
+                        <Text style={{ color: 'white', fontWeight: 'bold', letterSpacing: '1px' }}>
+                            Start Meeting
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
     )
 }
 
+const styles = StyleSheet.create({
+    info: {
+        backgroundColor: '#545454',
+        width: '100%',
+        height: 50,
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderColor: '#686868',
+        padding: 12,
+        // justifyContent: 'center',
+
+    },
+    textInput: {
+        // backgroundColor: 'green',
+        color: 'white',
+        fontSize: '20',
+    },
+    startMeetingBtn: {
+        width: '80vw',
+        marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'green',
+        height: 50,
+        borderRadius: 15,
+    },
+})
 
--- screens/MeetingRoom.js
+++ screens/MeetingRoom.js
@@ -1,45 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import StartMeeting from '../componets/StartMeeting'
+import { io } from "socket.io-client"
+// import {withSocket} from 'react-socket-io-client'
 
 export default function MeetingRoom() {
-
     const [name, setName] = useState()
     const [roomId, setRoomId] = useState()
+
+    useEffect(() => {
+        const API_URL = 'http://192.168.86.27'
+        socket = io(`${API_URL}`);
+        socket.on('connect', () => {
+            console.log('connected')
+        })
+    }, [])
 
     return (
         <View style={styles.container}>
             {/* New Meeting Input Fields */}
-            <View style={styles.startMeetingsContainer}>
-                <View style={styles.info}>
-                    <TextInput
-                        style={styles.textInput}
-                        value={name}
-                        placeholder='Enter Name'
-                        placeholderTextColor='#9b9b9b'
-                        onChangeText={text => setName(text)}
-                    />
-                </View>
-                <View style={styles.info}>
-                    <TextInput
-                        style={styles.textInput}
-                        value={roomId}
-                        placeholder='Enter A Room ID'
-                        placeholderTextColor='#9b9b9b'
-                        onChangeText={text => setRoomId(text)}
-                    />
-                </View>
-                {/* Button */}
-                <View style={{ alignItems: 'center' }}>
-                    <TouchableOpacity
-                        style={styles.startMeetingBtn}
-                        onPress={() => { }}
-                    >
-                        <Text style={{ color: 'white', fontWeight: 'bold', letterSpacing: '1px' }}>
-                            Start Meeting
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
+            <StartMeeting
+                name={name}
+                setName={setName}
+                roomId={roomId}
+                setRoomId={setRoomId}
+            />
         </View>
     )
 }
@@ -52,29 +37,4 @@ const styles = StyleSheet.create({
     startMeetingsContainer: {
         // backgroundColor: '#ea1e32'
     },
-    info: {
-        backgroundColor: '#545454',
-        width: '100%',
-        height: 50,
-        borderTopWidth: 1,
-        borderBottomWidth: 1,
-        borderColor: '#686868',
-        padding: 12,
-        // justifyContent: 'center',
-
-    },
-    textInput: {
-        // backgroundColor: 'green',
-        color: 'white',
-        fontSize: '20',
-    },
-    startMeetingBtn: {
-        width: '80vw',
-        marginTop: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'green',
-        height: 50,
-        borderRadius: 15,
-    },
-})
+});
